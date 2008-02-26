25a7674aafe934708956df35df7b0469fbc2d363
Necoro <>
2008-02-26 20:36:55 +0000

    r798@Devoty:  necoro | 2008-02-26 21:36:42 +0100  Removed test

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@
 #
 # Written by René 'Necoro' Neumann <necoro@necoro.net>
 
-#test
-
 import sys, os, os.path
 from distutils.core import setup, Extension
 from portato.constants import FRONTENDS, VERSION, DATA_DIR, ICON_DIR, PLUGIN_DIR, TEMPLATE_DIR
