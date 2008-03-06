3cb720e586e9956697d03897bf815e318950d86f
René 'Necoro' Neumann <necoro@necoro.net>
2008-03-06 22:33:11 +0100

    Update createpot.sh

--- createpot.sh
+++ createpot.sh
@@ -3,7 +3,6 @@
 # Create the .pot file
 # Thanks to porthole for inspiration ;)
 
-files=$(find -name "*.py")
-xgettext -k_ -kN_ -L glade -o i18n/messages.pot  portato/gui/templates/portato.glade
-xgettext -k_ -kN_ -j --from-code=UTF-8 -o i18n/messages.pot $files
+xgettext -k_ -kN_ -L glade -o i18n/messages.pot $(find -name "*.glade")
+xgettext -k_ -kN_ -j --from-code=UTF-8 -o i18n/messages.pot $(find -name "*.py")
 
