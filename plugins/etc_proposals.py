e42090a8bd7dcc5f9e7c6e41b0f44698588e6586
René 'Necoro' Neumann <necoro@necoro.net>
2008-07-03 23:33:21 +0200

    Ported etc-proposals plugin

---
+++ plugins/etc_proposals.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# File: plugins/etc_proposals.py
+# This file is part of the Portato-Project, a graphical portage-frontend.
+#
+# Copyright (C) 2007-2008 René 'Necoro' Neumann
+# This is free software.  You may redistribute copies of it under the terms of
+# the GNU General Public License version 2.
+# There is NO WARRANTY, to the extent permitted by law.
+#
+# Written by René 'Necoro' Neumann <necoro@necoro.net>
+
+from portato.helper import error
+
+import os
+from subprocess import Popen
+
+class EtcProposals (Plugin):
+	__author__ = "René 'Necoro' Neumann"
+	__description__ = "Adds support for <b>etc-proposals</b>, a graphical etc-update replacement."
+	__dependency__ = "app-portage/etc-proposals"
+
+	def __init__ (self):
+		Plugin.__init__(self)
+
+		self.prog = ["/usr/sbin/etc-proposals"]
+		self.add_call("after_emerge", self.hook, type = "after")
+		self.add_menu("Et_c-Proposals", self.menu)
+
+	def launch (self, options = []):
+		if os.getuid() == 0:
+			Popen(self.prog+options)
+		else:
+			error("ETC_PROPOSALS :: %s",_("Cannot start etc-proposals. Not root!"))
+
+	def hook (self, *args, **kwargs):
+		"""Entry point for this plugin."""
+		self.launch(["--fastexit"])
+
+	def menu (self, *args):
+		self.launch()
+
+register(EtcProposals)
