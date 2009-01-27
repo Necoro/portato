fb06f577bae6bf02a6c585cd78c31d065e0e98dc
René 'Necoro' Neumann <necoro@necoro.net>
2009-01-27 16:43:20 +0100

    Move gettext init stuff from portato.py to portato/__init__.py

--- portato/__init__.py
+++ portato/__init__.py
@@ -3,7 +3,7 @@
 # File: portato/__init__.py
 # This file is part of the Portato-Project, a graphical portage-frontend.
 #
-# Copyright (C) 2006-2007 René 'Necoro' Neumann
+# Copyright (C) 2006-2009 René 'Necoro' Neumann
 # This is free software.  You may redistribute copies of it under the terms of
 # the GNU General Public License version 2.
 # There is NO WARRANTY, to the extent permitted by law.
@@ -13,6 +13,12 @@
 from __future__ import absolute_import
 
 from . import log
+import gettext, locale
+from portato.constants import LOCALE_DIR, APP
+
+# set gettext stuff
+locale.setlocale(locale.LC_ALL, '')
+gettext.install(APP, LOCALE_DIR, unicode = True)
 
 # start logging
 log.start(file=False)
