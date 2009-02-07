4e8514ada1f3095c142e3a72eaaa325f5aca031f
René 'Necoro' Neumann <necoro@necoro.net>
2009-02-07 22:23:45 +0100

    Same copyright statement in all files

--- portato/backend/portage/settings.py
+++ portato/backend/portage/settings.py
@@ -3,7 +3,7 @@
 # File: portato/backend/portage/settings.py
 # This file is part of the Portato-Project, a graphical portage-frontend.
 #
-# Copyright (C) 2006-2007 René 'Necoro' Neumann
+# Copyright (C) 2006-2009 René 'Necoro' Neumann
 # This is free software.  You may redistribute copies of it under the terms of
 # the GNU General Public License version 2.
 # There is NO WARRANTY, to the extent permitted by law.
