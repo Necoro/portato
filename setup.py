0235295d89cf6f8f1a9e9c4005505d41690f97cb
René 'Necoro' Neumann <necoro@necoro.net>
2009-02-17 19:59:57 +0100

    It is always a nice idea to install the stuff you need....

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def plugin_list (*args):
     """Creates a list of correct plugin pathes out of the arguments."""
     return [("plugins/%s.py" % x) for x in args]
 
-packages = ["portato", "portato.gui", "portato.gui.windows", "portato.plugins", "portato.backend", "portato.backend.portage"]
+packages = ["portato", "portato.db", "portato.gui", "portato.gui.windows", "portato.plugins", "portato.backend", "portato.backend.portage"]
 data_files = [
         (TEMPLATE_DIR, [os.path.join("portato/gui/templates",x) for x in os.listdir("portato/gui/templates") if x.endswith(".glade")]),
         (ICON_DIR, ["icons/portato-icon.png"]),
