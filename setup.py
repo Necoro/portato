0b50ae98f1e54a9e24dfc6ebc38c2a891bfa931e
René 'Necoro' Neumann <necoro@necoro.net>
2009-08-15 00:22:34 +0200

    Also allow --enable-eix

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ else:
     cmdclass['build_ext'] = build_ext
     packages.append("portato.eix")
 
-    if "--enable--eix" in sys.argv:
+    if "--enable-eix" in sys.argv:
         sys.argv.remove("--enable-eix")
 
 # do the distutils setup
