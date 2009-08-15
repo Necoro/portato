20f6015a96331575b3b402425f4c42eb91912cf1
René 'Necoro' Neumann <necoro@necoro.net>
2009-08-15 15:19:01 +0200

    Enhanced the extensions.sh

--- extensions.sh
+++ extensions.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-python setup.py build_ext -i
-# remove the "build" directory
-python setup.py clean -a 
+cflags=$(python -c "from portato.backend import system; print system.get_global_settings('CFLAGS')")
+
+find -name "*.so" -print0 | xargs -0 rm -f
+find -name "*.c" -print0 | xargs -0 rm -f
+
+CFLAGS=$cflags python setup.py build_ext -i
+
+rm -rf build
