ba2e2f270cd457c3897465eb877b72a6012ac702
René 'Necoro' Neumann <necoro@necoro.net>
2010-03-05 16:17:03 +0100

    Fixed copyright

--- portato/db/eix_sql.py
+++ portato/db/eix_sql.py
@@ -3,7 +3,7 @@
 # File: portato/db/eix_sql.py
 # This file is part of the Portato-Project, a graphical portage-frontend.
 #
-# Copyright (C) 2006-2009 René 'Necoro' Neumann
+# Copyright (C) 2006-2010 René 'Necoro' Neumann
 # This is free software.  You may redistribute copies of it under the terms of
 # the GNU General Public License version 2.
 # There is NO WARRANTY, to the extent permitted by law.
