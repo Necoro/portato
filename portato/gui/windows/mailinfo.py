c8fab8cd56c88d2322104afc960c836a543cd3d6
René 'Necoro' Neumann <necoro@necoro.net>
2010-03-14 20:02:03 +0100

    Send more files for bug mails

--- portato/gui/windows/mailinfo.py
+++ portato/gui/windows/mailinfo.py
@@ -14,15 +14,41 @@ from __future__ import absolute_import, with_statement
 
 import smtplib, socket
 import time
+import gtk, pango
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from os.path import basename
 
 from .basic import AbstractDialog
 from ..utils import GtkThread
 from ..dialogs import mail_failure_dialog
 from ...helper import debug, info
-from ...constants import VERSION
+from ...constants import VERSION, CONFIG_LOCATION
 from ...log import LOGFILE
+from ... import session
+
+class ShowDialog (gtk.Dialog):
+
+    def __init__(self, parent, f):
+        gtk.Dialog.__init__(self, f, parent, buttons = (gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
+
+        textview = gtk.TextView()
+        textview.set_editable(False)
+        textview.modify_font(pango.FontDescription("Monospace"))
+        textview.set_size_request(gtk.gdk.screen_width()/2, gtk.gdk.screen_height()/3)
+    
+        sw = gtk.ScrolledWindow();
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        sw.add(textview)
+        
+        self.vbox.add(sw)
+
+        textbuffer = textview.get_buffer()
+
+        with open(f) as text:
+            textbuffer.set_text(text.read())
+
+        self.vbox.show_all()
 
 class MailInfoWindow (AbstractDialog):
     TO = "bugs@portato.necoro.net"
@@ -30,9 +56,41 @@ class MailInfoWindow (AbstractDialog):
     def __init__ (self, parent, tb):
 
         AbstractDialog.__init__(self, parent)
+
+        self.files = [LOGFILE, CONFIG_LOCATION] + [s._file for s in session.sessionlist]
+        self.fileList = self.tree.get_widget("fileList")
+        self.build_file_list()
         
         self.tb = tb
         self.window.show_all()
+
+    def build_file_list(self):
+        store = gtk.ListStore(bool, str)
+
+        for f in self.files:
+            store.append((True, f))
+
+        self.fileList.set_model(store)
+        cell = gtk.CellRendererText()
+        tCell = gtk.CellRendererToggle()
+        tCell.set_property("activatable", True)
+        tCell.connect("toggled", self.cb_file_toggled)
+
+        self.fileList.append_column(gtk.TreeViewColumn(None, tCell, active = 0))
+        self.fileList.append_column(gtk.TreeViewColumn(None, cell, text = 1))
+
+    def cb_file_toggled(self, cell, path):
+        store = self.fileList.get_model()
+        store[path][0] = not store[path][0]
+        return True
+
+    def cb_file_clicked(self, view, path, *args):
+        store = view.get_model()
+        f = store[path][1]
+        
+        dialog = ShowDialog(self.window, f)
+        dialog.run()
+        dialog.destroy()
 
     def set_data (self):
         self.message = MIMEMultipart()
@@ -63,13 +121,15 @@ class MailInfoWindow (AbstractDialog):
         txtmsg = MIMEText(text, "plain", "utf-8")
         self.message.attach(txtmsg)
 
-        # log
-        if self.tree.get_widget("logCheck").get_active():
-            with open(LOGFILE, "r") as f:
-                log = MIMEText(f.read(), "plain", "utf-8")
-                log.add_header('Content-Disposition', 'attachment', filename='portato.log')
+        # logs
+        for (active, f) in self.fileList.get_model():
+            if active:
+                debug("Attaching '%s'", f)
+                with open(f, "r") as text:
+                    log = MIMEText(text.read(), "plain", "utf-8")
+                    log.add_header('Content-Disposition', 'attachment', filename=basename(f))
 
-            self.message.attach(log)
+                self.message.attach(log)
 
     def send (self):
         try:
@@ -77,21 +137,22 @@ class MailInfoWindow (AbstractDialog):
             server = smtplib.SMTP("mail.necoro.eu")
             debug("Sending mail")
             try:
-                try:
-                    server.sendmail(self.addr, self.TO, self.message.as_string())
-                except smtplib.SMTPRecipientsRefused, e:
-                    info(_("An error occurred while sending. I think we were greylisted. The error: %s") % e)
-                    info(_("Retrying after waiting 60 seconds."))
-                    time.sleep(60)
-                    server.sendmail(self.addr, self.TO, self.message.as_string())
-                debug("Sent")
+                for i in range(5): # try 5 times at max
+                    try:
+                        server.sendmail(self.addr, self.TO, self.message.as_string())
+                    except smtplib.SMTPRecipientsRefused, e:
+                        info(_("An error occurred while sending. I think we were greylisted. The error: %s") % e)
+                        info(_("Retrying after waiting %d seconds."), 30)
+                        time.sleep(30)
+                    else:
+                        debug("Sent")
+                        break
             finally:
                 server.quit()
         except socket.error, e:
             mail_failure_dialog("%s (Code: %s)" % (e.args[1], e.args[0]))
         
     def cb_cancel_clicked (self, *args):
-
         self.close()
         return True
 
