0d422b9c17b5aaf7c59a2482f3dc8009189844c7
René 'Necoro' Neumann <necoro@necoro.net>
2010-04-10 00:07:18 +0200

    Fix createpot.sh

--- createpot.sh
+++ createpot.sh
@@ -4,5 +4,6 @@
 # Thanks to porthole for inspiration ;)
 
 xgettext -k_ -kN_ -L glade -o i18n/messages.pot $(find -name "*.ui")
+xgettext -k_ -kN_ -j -L glade -o i18n/messages.pot $(find -name "*.menu")
 xgettext -k_ -kN_ -j --from-code=UTF-8 -o i18n/messages.pot $(find -name "*.py")
 
